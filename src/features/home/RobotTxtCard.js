265fbb75414dd37bf1651c2281b53c6a417ea947
Luca <mistalbo@live.com>
2018-04-25 21:32:32 +0200

    Added Delete icon in RobotTxtCard

--- src/features/home/RobotTxtCard.js
+++ src/features/home/RobotTxtCard.js
@@ -2,16 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import classnames from 'classnames';
-import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import { Card, CardHeader, CardContent, CardActions, IconButton, Typography } from 'material-ui';
 import Collapse from 'material-ui/transitions/Collapse';
-import Avatar from 'material-ui/Avatar';
-import IconButton from 'material-ui/IconButton';
-import Typography from 'material-ui/Typography';
 import red from 'material-ui/colors/red';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const styles = theme => ({
   card: {
@@ -58,37 +53,9 @@ class RobotTxtCard extends Component {
       <div className="home-robot-txt-card">
         <Card className={classes.card}>
           <CardHeader
-            // avatar={
-            //   <Avatar aria-label="Recipe" className={classes.avatar}>
-            //     R
-            //   </Avatar>
-            // }
-            // action={
-            //   <IconButton>
-            //     <MoreVertIcon />
-            //   </IconButton>
-            // }
             title={this.props.website.robot_url}
-            // subheader="September 14, 2016"
           />
-          {/* <CardMedia
-            className={classes.media}
-            image="/static/images/cards/paella.jpg"
-            title="Contemplative Reptile"
-          /> */}
-          {/* <CardContent>
-            <Typography component="p">
-              This impressive paella is a perfect party dish and a fun meal to cook together with
-              your guests. Add 1 cup of frozen peas along with the mussels, if you like.
-            </Typography>
-          </CardContent> */}
           <CardActions className={classes.actions} disableActionSpacing>
-            {/* <IconButton aria-label="Add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="Share">
-              <ShareIcon />
-            </IconButton> */}
             <IconButton
               className={classnames(classes.expand, {
                 [classes.expandOpen]: this.state.expanded,
@@ -115,6 +82,11 @@ class RobotTxtCard extends Component {
                 robot_url: {this.props.website.robot_url}
               </Typography>
             </CardContent>
+            <CardActions className={classes.actions}>
+              <IconButton>
+                <DeleteIcon />
+              </IconButton>
+            </CardActions>
           </Collapse>
         </Card>
       </div>
